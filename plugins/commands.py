3aabe0ff1e2d27b6c496c6a866e8cd39f877a057
Ian Waithaka <iangigs69@gmail.com>
2021-07-01 13:01:25 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,6 +19,9 @@ async def start(bot, message):
         buttons = [[
             InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Go Inline', switch_inline_query=''),
+                                    ],
+        [
+        InlineKeyboardButton('👉 Join starlabs channel 👈', url='t.me/TGstarlabs'),
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(START_MSG, reply_markup=reply_markup)
