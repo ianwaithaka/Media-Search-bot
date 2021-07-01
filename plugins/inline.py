d05c55cab090be8cc3149b2ca51da45f839c8726
Ian Waithaka <iangigs69@gmail.com>
2021-07-01 12:50:27 +0300

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -74,7 +74,9 @@ def get_reply_markup(username, query):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
     buttons = [[
         InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-        InlineKeyboardButton('Share bot', url=url),
+                        ],
+        [
+        InlineKeyboardButton('👉 Join starlabs channel 👈', url='t.me/TGMovies254'),
     ]]
     return InlineKeyboardMarkup(buttons)
 
